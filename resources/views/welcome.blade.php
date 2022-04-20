c93c83088f43c073230844516b674cd8e515668b
JJuanEH <jose.escobar@jpyrsa.mx>
2022-04-20 16:24:12 -0500

    nueva linea

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <div class="container py-5 text-center">
         <h1>Hola Mundo</h1>
         <input type="button" class="btn btn-primary" value="entrar">
+        <input type="button" class="btn btn-primary" value="entrar">
     </div>
 
 
