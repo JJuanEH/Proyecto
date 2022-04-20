3bfaaf5512fcb448a2eef4c83bca0fc361fbaf33
josbray <101306720+josbray@users.noreply.github.com>
2022-04-20 17:54:56 -0500

    ey

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
 
     <div class="container py-5 text-center">
         <h1>Hola Mundo</h1>
+
+        <h3>hola</h3>
         <input type="button" class="btn btn-primary" value="entrar">
         <input type="button" class="btn btn-primary" value="entrar">
     </div>
