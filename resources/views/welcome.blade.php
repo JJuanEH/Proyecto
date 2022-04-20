de441c158172601ff1998b1e7dd5818547f0c438
josbray <101306720+josbray@users.noreply.github.com>
2022-04-20 17:50:50 -0500

    mens

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
 
     <div class="container py-5 text-center">
         <h1>Hola Mundo</h1>
+        <h2>que onda</h2>
         <input type="button" class="btn btn-primary" value="entrar">
         <input type="button" class="btn btn-primary" value="entrar">
     </div>
