e50ddf5076ba4e9a68fb3eb569298581f91d68b4
JJuanEH <jose.escobar@jpyrsa.mx>
2022-04-20 16:04:59 -0500

    Primer comentario

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('base');
+
+@section('content')
+
+    <div class="container py-5 text-center">
+        <h1>Hola Mundo</h1>
+        <input type="button" class="btn btn-primary" value="entrar">
+    </div>
+
+
+@endsection
